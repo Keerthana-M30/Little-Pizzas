212c35990ba74bff7ac8e86035bd56ab87d2c2d9
keerthana30m <2k21aids28@kiot.ac.in>
2025-10-17 17:50:04 +0530

    contact number with code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,7 @@ import { MenuCategorySection } from "@/components/MenuCategory";
 import { MENU_CATEGORIES } from "@/menuData";
 
 // Shop owner's WhatsApp number (with country code, without '+')
-const SHOP_OWNER_PHONE = "8098511211";
+const SHOP_OWNER_PHONE = "918098511211";
 
 const Index = () => {
   const [order, setOrder] = useState<Record<string, number>>({});
